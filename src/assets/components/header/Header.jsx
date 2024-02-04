2644a0e64d66e1f7ee2bd50992e1bfdc8fb0944f
nivorako <nivo.rakoto@yahoo.fr>
2024-02-04 02:45:09 +0100

    set drawerOnClick on menuIcon

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -11,7 +11,14 @@ const Header = () => {
 
     const isTablette = useMediaQuery("(max-width:1024px)");
     const isMobile = useMediaQuery("(max-width:768px)");
-    const navItems = ["location", "formation", "gestion planning", "business booster"]
+    const navItems = ["location", "formation", "gestion planning", "business booster"];
+    const drawerOnClick = () => {
+        const menuIcon = document.getElementById("menuIcon");
+        const drawer = document.getElementById("drawer")
+        if(menuIcon && drawer){
+            drawer.style.display = "block";
+        }
+    }
 
     return (
         <header
@@ -43,18 +50,20 @@ const Header = () => {
                                 tabIndex="0"
                                 aria-label="Menu Toggle"
                                 aria-expanded="false"
+                                onClick={drawerOnClick}
                             >
-                            <div className="menuIconContainer">
-                                <svg
-                                    aria-hidden="true"
-                                    role="presentation"
-                                    className="menuIcon"
-                                    viewBox="0 0 1000 1000"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M104 333H896C929 333 958 304 958 271S929 208 896 208H104C71 208 42 237 42 271S71 333 104 333ZM104 583H896C929 583 958 554 958 521S929 458 896 458H104C71 458 42 487 42 521S71 583 104 583ZM104 833H896C929 833 958 804 958 771S929 708 896 708H104C71 708 42 737 42 771S71 833 104 833Z"></path>
-                                </svg>
-                            </div>
+                                <div className="menuIconContainer">
+                                    <svg
+                                        aria-hidden="true"
+                                        role="presentation"
+                                        className="menuIcon"
+                                        id="menuIcon"
+                                        viewBox="0 0 1000 1000"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M104 333H896C929 333 958 304 958 271S929 208 896 208H104C71 208 42 237 42 271S71 333 104 333ZM104 583H896C929 583 958 554 958 521S929 458 896 458H104C71 458 42 487 42 521S71 583 104 583ZM104 833H896C929 833 958 804 958 771S929 708 896 708H104C71 708 42 737 42 771S71 833 104 833Z"></path>
+                                    </svg>
+                                </div>
                             </div>
                         ) : (
                             <div 
@@ -63,12 +72,14 @@ const Header = () => {
                                 tabIndex="0"
                                 aria-label="Menu Toggle"
                                 aria-expanded="false"
+                                onClick={drawerOnClick}
                             >
                             
                                 <svg
                                     aria-hidden="true"
                                     role="presentation"
                                     className="menuIcon"
+                                    id="menuIcon"
                                     viewBox="0 0 1000 1000"
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
@@ -169,8 +180,14 @@ const Header = () => {
                         </div>  
                     )
                 }
-                             
+            
             </section>
+            <div 
+                className="drawer"
+                id="drawer"
+            >
+                Draweeer
+            </div>
         </header>
     );
 };
