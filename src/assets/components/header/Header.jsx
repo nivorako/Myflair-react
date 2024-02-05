68d6e934dcd1cd64cf610363f12d0530bef5e818
nivorako <nivo.rakoto@yahoo.fr>
2024-02-05 11:45:03 +0100

    set toggle drawer in menuIcon onclick

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { useMediaQuery } from 'usehooks-ts';
+import MenuIcon from "../menuIcon/MenuIcon";
+import IconClose from "../iconClose/IconClose";
 import "./index.css";
 
 /**
@@ -12,12 +14,17 @@ const Header = () => {
     const isTablette = useMediaQuery("(max-width:1024px)");
     const isMobile = useMediaQuery("(max-width:768px)");
     const navItems = ["location", "formation", "gestion planning", "business booster"];
+    const [isDrawer, toggleDrawer] = useState(false);
+
     const drawerOnClick = () => {
         const menuIcon = document.getElementById("menuIcon");
         const drawer = document.getElementById("drawer")
-        if(menuIcon && drawer){
-            drawer.style.display = "block";
-        }
+        toggleDrawer(!isDrawer)
+            if(!isDrawer){
+                drawer.style.display="block"
+            }else{
+                drawer.style.display="none"
+            }
     }
 
     return (
@@ -53,16 +60,10 @@ const Header = () => {
                                 onClick={drawerOnClick}
                             >
                                 <div className="menuIconContainer">
-                                    <svg
-                                        aria-hidden="true"
-                                        role="presentation"
-                                        className="menuIcon"
-                                        id="menuIcon"
-                                        viewBox="0 0 1000 1000"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path d="M104 333H896C929 333 958 304 958 271S929 208 896 208H104C71 208 42 237 42 271S71 333 104 333ZM104 583H896C929 583 958 554 958 521S929 458 896 458H104C71 458 42 487 42 521S71 583 104 583ZM104 833H896C929 833 958 804 958 771S929 708 896 708H104C71 708 42 737 42 771S71 833 104 833Z"></path>
-                                    </svg>
+                                    {
+                                        !isDrawer ? <MenuIcon /> : <IconClose />
+                                    }
+                                    
                                 </div>
                             </div>
                         ) : (
@@ -74,18 +75,9 @@ const Header = () => {
                                 aria-expanded="false"
                                 onClick={drawerOnClick}
                             >
-                            
-                                <svg
-                                    aria-hidden="true"
-                                    role="presentation"
-                                    className="menuIcon"
-                                    id="menuIcon"
-                                    viewBox="0 0 1000 1000"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M104 333H896C929 333 958 304 958 271S929 208 896 208H104C71 208 42 237 42 271S71 333 104 333ZM104 583H896C929 583 958 554 958 521S929 458 896 458H104C71 458 42 487 42 521S71 583 104 583ZM104 833H896C929 833 958 804 958 771S929 708 896 708H104C71 708 42 737 42 771S71 833 104 833Z"></path>
-                                </svg>
-                            
+                                {
+                                    !isDrawer ? <MenuIcon /> : <IconClose />
+                                }
                             </div>
                         )
                         
@@ -119,38 +111,6 @@ const Header = () => {
                         </ul>
                     )
                 } 
-
-                {/* <div
-                    className="elementor-menu-toggle"
-                    role="button"
-                    tabIndex="0"
-                    aria-label="Menu Toggle"
-                    aria-expanded="false"
-                >
-                    <svg
-                        aria-hidden="true"
-                        role="presentation"
-                        className="elementor-menu-toggle__icon--open elementor-animation-grow e-font-icon-svg e-eicon-menu-bar"
-                        viewBox="0 0 1000 1000"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="M104 333H896C929 333 958 304 958 271S929 208 896 208H104C71 208 42 237 42 271S71 333 104 333ZM104 583H896C929 583 958 554 958 521S929 458 896 458H104C71 458 42 487 42 521S71 583 104 583ZM104 833H896C929 833 958 804 958 771S929 708 896 708H104C71 708 42 737 42 771S71 833 104 833Z"></path>
-                    </svg>
-                    <svg
-                        aria-hidden="true"
-                        role="presentation"
-                        className="elementor-menu-toggle__icon--close elementor-animation-grow e-font-icon-svg e-eicon-close"
-                        viewBox="0 0 1000 1000"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="M742 167L500 408 258 167C246 154 233 150 217 150 196 150 179 158 167 167 154 179 150 196 150 212 150 229 154 242 171 254L408 500 167 742C138 771 138 800 167 829 196 858 225 858 254 829L496 587 738 829C750 842 767 846 783 846 800 846 817 842 829 829 842 817 846 804 846 783 846 767 842 750 829 737L588 500 833 258C863 229 863 200 833 171 804 137 775 137 742 167Z"></path>
-                    </svg>
-                    <span className="elementor-screen-only">
-                        Menu
-                    </span>
-                    
-                </div> */}
-
                 
                 {/* header-connect */}
                 {
