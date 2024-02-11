6d29e0f5fce05fd68a141ad5d8910746b1f6c3be
nivorako <nivo.rakoto@yahoo.fr>
2024-02-11 22:27:06 +0100

    set up section3

--- src/assets/components/section3/Section3.jsx
+++ src/assets/components/section3/Section3.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import "./index.css";
 
-const Section3 = () => {
+const Section3 = ({
+    id, 
+    title, 
+    section3List,
+}) => {
     return (
-        <div>
-            Service section
-        </div>
+        <section className='section3' id={id}>
+             <h2 className='section3-title'>
+                {title}
+            </h2>
+            <div className='section3-cards'>
+                {
+                    section3List.map((card, index) => {
+                        return(
+                            <div
+                                className='section3-card'
+                                key={index}
+                            >
+                                <div className='section3-cardImg'>
+                                    <img src={card.img} alt="toto" />
+                                </div>
+                                <div className='section3-cardDesc'>
+                                    <div className='section3-cardTitle'>
+                                        <h3>{card.title}</h3>
+                                        <p>{card.price} &#8364; / jour</p>
+                                    </div>
+                                    <button className='section3-btn'>
+                                        <p
+                                            className='section3-BtnText'
+                                        >
+                                            Ajoutez au panier
+                                        </p>
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section >
     )
 }
 
--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Banner from "../../components/banner/Banner";
 import Section from "../../components/section/Section";
 import Section2 from "../../components/section2/Section2";
+import Section3 from "../../components/section3/Section3";
 
 import "./index.css";
 const Home = () => {
@@ -34,7 +35,7 @@ const Home = () => {
             img:"images/2024-01-0H7A3960-300x200.webp",
             btnText:["Onglerie"],
             title:"Titre business 2",
-            price: "100"
+            price: "1010"
         },{
             img:"images/2024-01-0H7A5060-768x512.webp",
             btnText:["Cabines"],
@@ -59,6 +60,35 @@ const Home = () => {
         }
     ]
 
+    const servicesList = [
+        {
+            img:"images/2024-01-0H7A3960-300x200.webp",
+            title:"Location Sèche Cheveux",
+            price: "10"
+        },{
+            img:"images/2024-01-0H7A3960-300x200.webp",
+            title:"Location fer à lisser",
+            price: "10"
+        },{
+            img:"images/2024-01-0H7A5060-768x512.webp",
+            title:"Serviette jetable",
+            price: "1"
+        },
+        {
+            img:"images/2024-01-0H7A3960-300x200.webp",
+            title:"Ajoutez votre titre ici",
+            price: "100"
+        },{
+            img:"images/2024-01-0H7A3960-300x200.webp",
+            title:"Ajoutez votre titre ici",
+            price: "100"
+        },{
+            img:"images/2024-01-0H7A5060-768x512.webp",
+            title:"Ajoutez votre titre ici",
+            price: "100"
+        }
+    ]
+
     return (
         <>
             <main id="home" className="home">
@@ -73,6 +103,11 @@ const Home = () => {
                         id="business"
                         title="Nos Business Booster"
                         section2List={businessList}
+                    />
+                    <Section3 
+                        id="services"
+                        title="Nos Services Supplémentaires"
+                        section3List={servicesList}
                     />
                 </div>
             </main>
