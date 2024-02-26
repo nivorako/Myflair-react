4dbfa28fb9bde5de0187421b87942c63324f2a1f
nivorako <nivo.rakoto@yahoo.fr>
2024-02-26 11:13:30 +0100

    modify math name in Home

--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import Section2 from "../../components/section2/Section2";
 import Section3 from "../../components/section3/Section3";
 import "./index.css";
 import WhatsappAd from "../../components/whatsappAd/WhatsappAd";
-import WhatsappModal from "../../components/whatsappModal/whatsappModal";
+import WhatsappModal from "../../components/whatsappModal/WhatsappModal";
 const Home = () => {
 
     const sectionList = [
